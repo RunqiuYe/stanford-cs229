4b9b7004fd8b6370962fb215bdbd84ef6c8ebad7
Runqiu_Ye <ye_runqiu@outlook.com>
2024-06-24 21:17:22 -0400

    finished ps1-2

--- homework/2018 hw1/src/p02cde_posonly.py
+++ homework/2018 hw1/src/p02cde_posonly.py
@@ -28,8 +28,38 @@ def main(train_path, valid_path, test_path, pred_path):
     # *** START CODE HERE ***
     # Part (c): Train and test on true labels
     # Make sure to save outputs to pred_path_c
+    x_train, t_train = util.load_dataset(train_path, label_col='t', add_intercept=True)
+    x_test, t_test = util.load_dataset(test_path, label_col='t', add_intercept=True)
+    clf = LogisticRegression()
+    clf.fit(x_train, t_train)
+    pred_c = clf.predict(x_test)
+    np.savetxt(pred_path_c, pred_c > 0.5, fmt='%.1f')
+
+    util.plot(x_test, t_test, clf.theta, "output/p02c.png")
+
     # Part (d): Train on y-labels and test on true labels
     # Make sure to save outputs to pred_path_d
+    x_train, y_train = util.load_dataset(train_path, label_col='y', add_intercept=True)
+    x_test, t_test = util.load_dataset(test_path, label_col='t', add_intercept=True)
+    new_clf = LogisticRegression()
+    new_clf.fit(x_train, y_train)
+    pred_d = new_clf.predict(x_test)
+    np.savetxt(pred_path_d, pred_d > 0.5, fmt='%.1f')
+
+    util.plot(x_test, t_test, new_clf.theta, "output/p02d.png")
+
     # Part (e): Apply correction factor using validation set and test on true labels
     # Plot and use np.savetxt to save outputs to pred_path_e
+    x_valid, y_valid = util.load_dataset(valid_path, label_col='y', add_intercept=True)
+    pred_e = new_clf.predict(x_valid)
+
+    V_pos = np.sum(y_valid == 1)
+    alpha = np.sum((new_clf.predict(x_valid)) * np.reshape(y_valid == 1, (-1,1))) / V_pos
+    rescaled_theta = new_clf.theta.copy()
+    rescaled_theta[0] += np.log(2/alpha - 1)
+    pred_e = new_clf.predict(x_test) / alpha
+    np.savetxt(pred_path_e, pred_e > 0.5, fmt='%.1f')
+    util.plot(x_test, t_test, rescaled_theta, "output/p02e.png")
+
+
     # *** END CODER HERE
