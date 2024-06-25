fa82e4be78ff53ca3d16f0a3fd4c82aedc1b660c
Runqiu Ye <runqiuy@andrew.cmu.edu>
2024-06-24 20:05:14 -0400

    finished ps1-1

--- homework/2018 hw1/src/.ipynb_checkpoints/p01b_logreg-checkpoint.py
+++ homework/2018 hw1/src/.ipynb_checkpoints/p01b_logreg-checkpoint.py
@@ -15,6 +15,11 @@ def main(train_path, eval_path, pred_path):
     x_train, y_train = util.load_dataset(train_path, add_intercept=True)
 
     # *** START CODE HERE ***
+    clf = LogisticRegression()
+    clf.fit(x_train, y_train)
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=True)
+    y_pred = clf.predict(x_eval)
+    np.savetxt(pred_path, y_pred > 0.5, fmt='%.1f')
     # *** END CODE HERE ***
 
 
@@ -35,6 +40,56 @@ class LogisticRegression(LinearModel):
             y: Training example labels. Shape (m,).
         """
         # *** START CODE HERE ***
+        
+        def h(theta, x):
+            """
+            calculate h based on theta and xi
+            
+            theta:  parameter. shape (n,)
+            x:      all traning example inputs. shape (m, n)
+            return: hypotheses for all traning example. shape (m,)
+            """
+            return 1 / (1 + np.exp(- np.dot(x, theta)))
+        
+        def gradient(theta, x, y):
+            """
+            calculate the gradient vector of J wrt to theta
+            
+            theta:  shape(n,)
+            x:      all training example inputs. shape (m, n)
+            y:      all training example labels. shape (m,)
+            return: gradient of J. shape (n,)
+            """
+            m, n = x.shape
+            y = np.reshape(y, (m, 1))
+            return - 1 / m * np.dot(x.T, y - h(theta, x))
+        
+        def hessian(theta, x):
+            """
+            calculate the hessian matrix of J wrt to theta
+            
+            theta:  shape(n,)
+            x:      all training example inputs. shape (m, n)
+            return: hessian matrix of J. shape (n,n)
+            """
+            m, n = x.shape
+            h_theta_x = np.reshape(h(theta, x), (-1, 1))
+            return 1 / m * np.dot(x.T, h_theta_x * (1-h_theta_x) * x)
+        
+        def next_theta(theta, x, y):
+            return theta - np.dot(np.linalg.inv(hessian(theta, x)), gradient(theta, x, y))
+        
+        m, n = x.shape
+        self.theta = np.zeros((n, 1))
+        
+        old_theta = self.theta
+        new_theta = next_theta(self.theta, x, y)
+        while np.linalg.norm(abs(old_theta - new_theta)) >= self.eps:
+            old_theta = new_theta
+            new_theta = next_theta(old_theta, x, y)
+        self.theta = new_theta
+        print(f'Fitting result: theta is {self.theta}')
+
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -47,4 +102,5 @@ class LogisticRegression(LinearModel):
             Outputs of shape (m,).
         """
         # *** START CODE HERE ***
+        return (1 / (1 + np.exp(- np.dot(x, self.theta))) >= 0.5).astype(int)
         # *** END CODE HERE ***
--- homework/2018 hw1/src/.ipynb_checkpoints/p01e_gda-checkpoint.py
+++ homework/2018 hw1/src/.ipynb_checkpoints/p01e_gda-checkpoint.py
@@ -16,6 +16,11 @@ def main(train_path, eval_path, pred_path):
     x_train, y_train = util.load_dataset(train_path, add_intercept=False)
 
     # *** START CODE HERE ***
+    clf = GDA()
+    theta = clf.fit(x_train, y_train)
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=False)
+    y_pred = clf.predict(x_eval)
+    util.plot(x_eval, y_eval, clf.theta)
     # *** END CODE HERE ***
 
 
@@ -36,9 +41,28 @@ class GDA(LinearModel):
             y: Training example labels. Shape (m,).
 
         Returns:
-            theta: GDA model parameters.
+            theta: GDA model parameters. Shape (n+1, 1)
         """
         # *** START CODE HERE ***
+        m,n = x.shape
+        np.reshape(y, (m, 1))
+        self.phi = np.sum(y == 1) / m
+        self.mu0 = (np.dot(x.T, y == 0)) / (np.sum(y == 0))
+        self.mu1 = (np.dot(x.T, y == 1)) / (np.sum(y == 1))
+        self.sigma = np.dot((x - self.mu1).T * (y == 1), (x - self.mu1)) + np.dot((x - self.mu0).T * (y == 1), (x - self.mu0))
+        self.sigma = self.sigma / m
+        
+        self.theta = np.zeros((n+1, 1))
+        theta = np.dot(np.linalg.inv(self.sigma), self.mu1 - self.mu0)
+        theta0 = 0.5 * np.dot(self.mu0.T, np.dot(np.linalg.inv(self.sigma), self.mu0)) 
+        theta0 -= 0.5 * np.dot(self.mu1.T, np.dot(np.linalg.inv(self.sigma), self.mu1)) 
+        theta0 += np.log((1-self.phi)/self.phi)
+        self.theta[0] = theta0
+        self.theta[1:] = np.reshape(theta, (-1,1))
+        self.theta0 = theta0
+        self.theta1 = theta
+        print(f'Fitting result: theta is {self.theta}')
+        return self.theta
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -51,4 +75,5 @@ class GDA(LinearModel):
             Outputs of shape (m,).
         """
         # *** START CODE HERE ***
+        return (1 / (1 + np.exp(-(np.dot(x, self.theta1) + self.theta0)))) >= 0.5
         # *** END CODE HERE
