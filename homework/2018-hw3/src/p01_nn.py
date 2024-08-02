81f4ddc9783155e075af69092099c59a34d69640
Runqiu_Ye <ye_runqiu@outlook.com>
2024-08-02 11:42:43 -0400

    finish ps3-1b

--- homework/2018-hw3/src/p01_nn.py
+++ homework/2018-hw3/src/p01_nn.py
@@ -7,20 +7,20 @@ def example_weights():
     """
     w = {}
 
-    w['hidden_layer_0_1'] = 0
+    w['hidden_layer_0_1'] = 0.5
     w['hidden_layer_1_1'] = 0
-    w['hidden_layer_2_1'] = 0
-    w['hidden_layer_0_2'] = 0
-    w['hidden_layer_1_2'] = 0
+    w['hidden_layer_2_1'] = -1
+    w['hidden_layer_0_2'] = 0.5
+    w['hidden_layer_1_2'] = -1
     w['hidden_layer_2_2'] = 0
-    w['hidden_layer_0_3'] = 0
-    w['hidden_layer_1_3'] = 0
-    w['hidden_layer_2_3'] = 0
+    w['hidden_layer_0_3'] = -4
+    w['hidden_layer_1_3'] = 1
+    w['hidden_layer_2_3'] = 1
 
     w['output_layer_0'] = 0
-    w['output_layer_1'] = 0
-    w['output_layer_2'] = 0
-    w['output_layer_3'] = 0
+    w['output_layer_1'] = 1
+    w['output_layer_2'] = 1
+    w['output_layer_3'] = 1
 
     return w
 
